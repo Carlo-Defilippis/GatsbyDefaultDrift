0fce8c390e4cc09be5f0b659694c3fa17d0c221e
carlo defilippis <carlo.defilippis@me.com>
2022-01-12 10:48:10 -0500

    Added drift.page when client changes paths, excluding first enter

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+    console.log("New pathname: ", location.pathname)
+    console.log("Old pathname: ", prevLocation ? prevLocation.pathname : "Clients first time entering site.")
+    if (prevLocation) {
+        if (typeof window !== "undefined" && window.drift) {
+            window.drift.page(location.pathname)
+            console.log("Drift.page has be executed!")
+            }
+    }
+  }
+
